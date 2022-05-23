cbb80430093add0048ec32971daa6d6eaed8d04a
Pandurang karmure <karmurep@gmail.com>
2022-05-23 23:44:39 +0530

    up-commit
    check msg nd implement

--- myProject/src/sample/Myupdate.java
+++ myProject/src/sample/Myupdate.java
@@ -3,7 +3,8 @@ package sample;
 public class Myupdate {
 
 	public static void main(String[] args) {
-		System.out.println("we say worlds to Hello Everyone");
+		System.out.println("we say worlds' to Hello Everyone");
+		System.out.println("lai easy ahe bhida phakt");
 
 	}
 
